11369d7df9fa01337314381497ccdcd17f2df275
Xalmon <xalmonoyefule@gmail.com>
2024-08-16 13:54:34 +0100

    create a springboot project from bash, push to git and auto create branches and push to dockerhub

---
+++ src/main/java/com.example.vanessa/vanessaApplication.java
@@ -0,0 +1,12 @@
+package com.example.vanessa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class vanessaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(vanessaApplication.class, args);
+  }
+}
---
+++ src/test/java/com.example.vanessa/vanessaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vanessa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class vanessaApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
